dd3f40a12eb57fc0608f8575f21bb2309dab131d
akaryweb <yamashita.akary@gmail.com>
2024-10-11 10:26:55 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>
 );
+
+setInterval(() => {
+  root.render(
+    <p>現在時刻：{(new Date()).toLocaleString()}</p>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
